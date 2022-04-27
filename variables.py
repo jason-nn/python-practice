bf3a3bf1d67a1d5b4de2bb6e980afd152c611a70
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-27 18:25:13 +0800

    Add variables lessons

---
+++ variables.py
@@ -0,0 +1,101 @@
+# print('Hello, World!')
+
+# if 5 > 2:
+#     print('Five is greater than two!')
+
+# ----------------------------------------
+
+# x = 5
+# y = 'Jason'
+
+# print(x)
+# print(y)
+# print(y * 3)
+
+# print(type(x))
+# print(type(y))
+
+# ----------------------------------------
+
+# x, y, z = 'orange', 'banana', 'cherry'
+
+# print(x)
+# print(y)
+# print(z)
+
+# ----------------------------------------
+
+# x = y = z = 'orange'
+
+# print(x)
+# print(y)
+# print(z)
+
+# ----------------------------------------
+
+# fruits = ['apple', 'banana', 'cherry']
+# x, y, z = fruits
+
+# print(x)
+# print(y)
+# print(z)
+
+# print(x, y, z)
+# print(x + y + z)
+
+# ----------------------------------------
+
+# a = 5
+# b = 'Jason'
+
+# print(a, b)
+
+# ----------------------------------------
+
+# x = 'apples'
+
+
+# def foo():
+#     x = 'oranges'
+#     print(x)
+
+
+# foo()
+# print(x)
+
+# ----------------------------------------
+
+# x = 'apples'
+
+
+# def foo():
+#     global x
+#     x = 'oranges'
+#     print(x)
+
+
+# foo()
+# print(x)
+
+# ----------------------------------------
+
+# x = 'apples'
+
+
+# def foo():
+#     x = 'oranges'
+
+
+# foo()
+# print(x)
+
+
+# def foo():
+#     global x
+#     x = 'oranges'
+
+
+# foo()
+# print(x)
+
+# ----------------------------------------
