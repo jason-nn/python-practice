463ad1596d0befb4dfa7d50e8779296956c22d02
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-27 21:00:59 +0800

    Add string lessons

---
+++ strings.py
@@ -0,0 +1,59 @@
+# import random
+
+# print(random.randrange(1, 10))
+
+# ----------------------------------------
+
+# a = '''Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua.'''
+
+# print(a)
+
+# print(a[0])
+
+# ----------------------------------------
+
+# for x in 'banana':
+#     print(x)
+
+# ----------------------------------------
+
+# print(len('banana'))
+
+# print('ba' in 'banana')
+
+# print('ba' not in 'banana')
+
+# ----------------------------------------
+
+# b = 'banana'
+
+# print(b[1:5])
+
+# print(b[:5])
+
+# print(b[1:])
+
+# print(b[-5:-1])
+
+# ----------------------------------------
+
+# a = 'Hello, World!'
+
+# print(a.upper())
+
+# print(a.lower())
+
+# print('  Hello, World!  '.strip())
+
+# print(a.replace('l', 'w'))
+
+# print(a.split(','))
+
+# print(a.count('l'))
+
+# ----------------------------------------
+
+print('We are the so-called \'Vikings\' from the north.')
