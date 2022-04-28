e3cafb5e1d7379ff05c651d1ecf7e37a71c7c39d
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-28 11:57:21 +0800

    Add booleans lessons

---
+++ booleans.py
@@ -0,0 +1,32 @@
+# print(10 > 9)
+# print(10 == 9)
+
+# ----------------------------------------
+
+# a = 200
+# b = 33
+
+# if b > a:
+#     print('b is greater than a')
+# else:
+#     print('b is not greater than a')
+
+# ----------------------------------------
+
+# x = 'Hello'
+# y = 15
+
+# print(bool(x))
+# print(bool(y))
+
+# ----------------------------------------
+
+# print(bool(0))
+# print(bool(''))
+# print(bool([]))
+# print(bool({}))
+
+# print(bool(-1))
+# print(bool('a'))
+# print(bool(['a']))
+# print(bool({'a': 1}))
