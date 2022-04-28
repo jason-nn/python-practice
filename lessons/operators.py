06c56ed1a101f7933fe88a2b2dcce058c1862b15
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-28 12:15:28 +0800

    Add operators lesson

---
+++ lessons/operators.py
@@ -0,0 +1,19 @@
+# a = 12
+# b = 5
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+# print(a ** b)
+# print(a // b)
+
+# ----------------------------------------
+
+# a = True
+# b = False
+
+# print(a and b)
+# print(a or b)
+# print(not b)
