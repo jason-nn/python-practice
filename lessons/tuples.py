ad9001a7ea187ac9e56d84c53d9730aee8ff38f4
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-29 00:55:02 +0800

    Add tuples lesson

---
+++ lessons/tuples.py
@@ -0,0 +1,51 @@
+# a = ('apple')
+# print(type(a))
+
+# a = ('apple',)
+# print(type(a))
+
+# ----------------------------------------
+
+# a = ('apple', 'banana', 'cherry')
+# b = list(a)
+
+# b[0] = 'mango'
+# a = tuple(b)
+
+# print(a)
+
+# ----------------------------------------
+
+# a = ('apple', 'banana', 'cherry')
+
+# a += ('hello', 'my')
+
+# print(a)
+
+# ----------------------------------------
+
+# a = ('apple', 'banana', 'cherry')
+
+# x, y, z = a
+
+# print(x, y, z)
+
+# ----------------------------------------
+
+# a = ('apple', 'banana', 'cherry')
+
+# for i in a:
+#     print(i)
+
+# for i in range(len(a)):
+#     print(i, a[i])
+
+# ----------------------------------------
+
+# a = ('apple', 'banana')
+# b = ('cherry', 'grapes')
+
+# c = a + b
+# print(c)
+
+# print(c * 2)
