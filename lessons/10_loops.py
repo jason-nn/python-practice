3762a6a7a9613e944e26c99b7cfc606b411e4be4
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-29 15:45:13 +0800

    Comment out code

--- lessons/10_loops.py
+++ lessons/10_loops.py
@@ -77,9 +77,9 @@
 
 # ----------------------------------------
 
-adj = ['red', 'big', 'tasty']
-fruits = ['apple', 'banana', 'cherry']
+# adj = ['red', 'big', 'tasty']
+# fruits = ['apple', 'banana', 'cherry']
 
-for x in adj:
-    for y in fruits:
-        print(x, y)
+# for x in adj:
+#     for y in fruits:
+#         print(x, y)
