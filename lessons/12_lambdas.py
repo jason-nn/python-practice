d6ad8f1987ddbd189c5af76f3d46d7889e04b1b7
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-29 16:11:32 +0800

    Add lambda lesson

---
+++ lessons/12_lambdas.py
@@ -0,0 +1,22 @@
+# foo = (lambda x: x + 10)
+
+# print(foo(2))
+
+# bar = (lambda x, y: x + y)
+
+# print(bar(3, 5))
+
+# ----------------------------------------
+
+# def foo(n):
+#     return lambda a: a * n
+
+
+# double = foo(2)
+# triple = foo(3)
+
+# print(double(6))
+# print(double(34))
+
+# print(triple(6))
+# print(triple(34))
