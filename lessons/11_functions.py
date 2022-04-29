153b9663319db499923f55ca8275dfd5588bbdad
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-29 16:04:32 +0800

    Add function lesson

---
+++ lessons/11_functions.py
@@ -0,0 +1,61 @@
+# def foo(a, b):
+#     print(a, b)
+
+
+# foo('hi', 'hello')
+
+# ----------------------------------------
+
+# def foo(first, second, third):
+#     print('first is', first)
+#     print('second is', second)
+#     print('third is', third)
+
+
+# foo(third='Jason 3', second='Jason 2', first='Jason 1')
+
+# ----------------------------------------
+
+# def foo(*params):
+#     print(params[-1])
+
+
+# foo('a', 'b', 'c', 'd', 'e')
+
+# ----------------------------------------
+
+# def foo(**params):
+#     print(params['fname'])
+
+
+# foo(fname='Naruto', lname='Uzumaki', village='Hidden Leaf')
+
+# ----------------------------------------
+
+# def hi(name='World'):
+#     print('Hello {}!'.format(name))
+
+
+# hi()
+# hi('Jason')
+
+# ----------------------------------------
+
+# def foo(n):
+#     return n * 2
+
+
+# print(3 + foo(2))
+
+# ----------------------------------------
+
+# def fib(n):
+#     if n == 1:
+#         return 0
+#     elif n == 2:
+#         return 1
+#     else:
+#         return fib(n-1) + fib(n-2)
+
+
+# print(fib(10))
