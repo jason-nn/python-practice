3579a0cfabfe94419fe68de2e13fec5a8f5ca47f
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-29 17:46:19 +0800

    Add math lesson

---
+++ lessons/18_math.py
@@ -0,0 +1,23 @@
+# foo = min(100, 5, 25, 30)
+# bar = min([30, -1, 12])
+
+# print(foo)
+# print(bar)
+
+# ----------------------------------------
+
+# print(abs(-7.25))
+
+# print(pow(3, 2))
+
+# ----------------------------------------
+
+# import math
+
+# print(math.sqrt(.25))
+
+# print(math.ceil(.25))
+
+# print(math.floor(.25))
+
+# print(math.pi)
