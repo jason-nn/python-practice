4658818b7e48d10a71e031386fdffa75843dde58
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-29 20:19:16 +0800

    Add exceptions lesson

---
+++ lessons/21_exceptions.py
@@ -0,0 +1,48 @@
+# try:
+#     print('Hi' + 1)
+# except:
+#     print('Error')
+
+# ----------------------------------------
+
+# try:
+#     print('Hi' + 1)
+# except TypeError:
+#     print('Type error')
+# except:
+#     print('Error')
+
+# ----------------------------------------
+
+# try:
+#     print('Hi')
+# except:
+#     print('Error')
+# else:
+#     print('No errors raised')
+
+# ----------------------------------------
+
+# try:
+#     print('Hi' + 1)
+#     # print('Hi')
+# except:
+#     print('Error')
+# else:
+#     print('No errors raised')
+# finally:
+#     print('Always prints')
+
+# ----------------------------------------
+
+# foo = -1
+
+# if foo < 0:
+#     raise Exception('No negative numbers')
+
+# ----------------------------------------
+
+# foo = 'Hi'
+
+# if type(foo) != int:
+#     raise TypeError('Only integers allowed')
