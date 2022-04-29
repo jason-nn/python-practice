a455ec73456754b4c9948491e33effb41a3b74b9
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-29 15:44:42 +0800

    Add loops lesson

---
+++ lessons/10_loops.py
@@ -0,0 +1,85 @@
+# i = 1
+# while i < 6:
+#     print(i)
+#     i += 1
+
+# ----------------------------------------
+
+# i = 1
+# while i < 6:
+#     print(i)
+#     if i == 3:
+#         break
+#     i += 1
+
+# ----------------------------------------
+
+# i = 0
+# while i < 5:
+#     i += 1
+#     if i == 3:
+#         continue
+#     print(i)
+
+# ----------------------------------------
+
+# i = 1
+# while i < 6:
+#     print(i)
+#     i += 1
+# else:
+#     print('this runs when the loop ends')
+
+# ----------------------------------------
+
+# for x in 'banana':
+#     print(x)
+
+# ----------------------------------------
+
+# for x in 'banana':
+#     print(x)
+#     if x == 'n':
+#         break
+
+# ----------------------------------------
+
+# for x in 'banana':
+#     if x == 'n':
+#         continue
+#     print(x)
+
+# ----------------------------------------
+
+# for x in range(6):
+#     print(x)
+
+# ----------------------------------------
+
+# for x in range(1, 6):
+#     print(x)
+
+# ----------------------------------------
+
+# for x in range(1, 11, 2):
+#     print(x)
+# else:
+#     print('this runs when the loop ends')
+
+# ----------------------------------------
+
+# for x in range(1, 11, 2):
+#     if x == 9:
+#         break
+#     print(x)
+# else:
+#     print('this doesn\'t run if loop is stopped by break')
+
+# ----------------------------------------
+
+adj = ['red', 'big', 'tasty']
+fruits = ['apple', 'banana', 'cherry']
+
+for x in adj:
+    for y in fruits:
+        print(x, y)
