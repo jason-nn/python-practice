84bed173bf95b96c0d625672bfc7e344054fab08
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-29 17:40:23 +0800

    Add dates lesson

---
+++ lessons/17_dates.py
@@ -0,0 +1,21 @@
+import datetime
+
+# ----------------------------------------
+
+# now = datetime.datetime.now()
+
+# print(now)
+
+# print(now.year)
+
+# print(now.strftime('%A'))
+
+# ----------------------------------------
+
+foo = datetime.datetime(2022, 7, 26)
+
+# print(foo)
+
+# print(foo.strftime('%B %d, %Y'))
+
+# # https://www.w3schools.com/python/python_datetime.asp
