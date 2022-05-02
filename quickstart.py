15d8580fbeedf31789ae00154364e8c3efdd7006
Jason Ho <jason.ho@obf.ateneo.edu>
2022-05-02 11:55:36 +0800

    Add quickstart

---
+++ quickstart.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Home Page'
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return f'Hello, {escape(name.title())}!'
+
+
+@app.route('/user/<username>')
+def show_user(username):
+    return f'User {escape(username)}'
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'Post {escape(post_id)}'
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f'Subpath {escape(subpath)}'
+
+
+@app.route('/json')
+def show_json():
+    return {
+        'first_name': 'Tanjiro',
+        'last_name': 'Kamado',
+    }
