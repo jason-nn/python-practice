bda8c1210414b1ec726be8da0c0daea2b02face2
Jason Ho <jason.ho@obf.ateneo.edu>
2022-08-13 00:06:52 +0800

    Add mechanical soup scraper

---
+++ scraping/03.py
@@ -0,0 +1,25 @@
+import mechanicalsoup
+
+# 1
+browser = mechanicalsoup.Browser()
+url = "http://olympus.realpython.org/login"
+login_page = browser.get(url)
+login_html = login_page.soup
+
+# 2
+form = login_html.select("form")[0]
+form.select("input")[0]["value"] = "zeus"
+form.select("input")[1]["value"] = "ThunderDude"
+
+# 3
+profiles_page = browser.submit(form, login_page.url)
+
+print(profiles_page.soup)
+
+links = profiles_page.soup.select("a")
+
+base_url = "http://olympus.realpython.org"
+for link in links:
+    address = base_url + link["href"]
+    text = link.text
+    print(f"{text}: {address}")
