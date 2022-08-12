1c4dabf6e03ffe0c26b175f6404c4c7e5bcd3512
Jason Ho <jason.ho@obf.ateneo.edu>
2022-08-12 23:32:17 +0800

    Add beautiful soup exercise

--- scraping/second.py
+++ scraping/second.py
@@ -20,11 +20,22 @@ print(text)
 images = soup.find_all('img')
 print(images)
 
-sources = [image['src'] for image in images]
-print(sources)
+image_sources = [image['src'] for image in images]
+print(image_sources)
 
 
 print(soup.title)
 print(soup.title.string)
 
 print(soup.find_all("img", src="/static/dionysus.jpg"))
+
+
+exercise_base_url = 'http://olympus.realpython.org'
+
+soup = get_soup_from_url(url=f'{exercise_base_url}/profiles')
+
+links = soup.find_all('a')
+print(links)
+
+sources = [exercise_base_url + link['href'] for link in links]
+print(sources)
