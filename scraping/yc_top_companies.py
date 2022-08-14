5975442a7996ded9426685299ef5d1a4d5f87357
Jason Ho <jason.ho@obf.ateneo.edu>
2022-08-14 16:22:48 +0800

    Add scraper for yc top companies

---
+++ scraping/yc_top_companies.py
@@ -0,0 +1,48 @@
+import mechanicalsoup
+
+browser = mechanicalsoup.Browser()
+
+private_companies_url = 'https://www.ycombinator.com/topcompanies'
+public_companies_url = 'https://www.ycombinator.com/topcompanies/public'
+
+
+def get_main_headquarters(headquarter):
+    return headquarter.split(';')[0]
+
+
+def get_headquarters_array_from_url(url):
+    page = browser.get(url)
+    headquarters = page.soup.find_all('td', {'class': 'headquarters'})
+    return [get_main_headquarters(headquarter.text) for headquarter in headquarters]
+
+
+def group_headquarters_from_list(list):
+    output = {}
+    for headquarter in list:
+        if headquarter in output.keys():
+            output[headquarter] += 1
+        else:
+            output[headquarter] = 1
+    return output
+
+
+private_companies_headquarters = get_headquarters_array_from_url(
+    private_companies_url)
+
+public_companies_headquarters = get_headquarters_array_from_url(
+    public_companies_url)
+
+
+grouped_private_companies_headquarters = group_headquarters_from_list(
+    private_companies_headquarters)
+
+grouped_public_companies_headquarters = group_headquarters_from_list(
+    public_companies_headquarters)
+
+print('PRIVATE COMPANIES HEADQUARTERS')
+print(grouped_private_companies_headquarters)
+
+print()
+
+print('PUBLIC COMPANIES HEADQUARTERS')
+print(grouped_public_companies_headquarters)
