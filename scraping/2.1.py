36c754c15893da1641fae620b842cad3d043aa35
Jason Ho <jason.ho@obf.ateneo.edu>
2022-08-20 00:07:03 +0800

    Generalized function

--- scraping/2.1.py
+++ scraping/2.1.py
@@ -9,28 +9,46 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 results = soup.find(id='ResultsContainer')
 
-job_elements = results.find_all('div', class_='card-content')
+# job_elements = results.find_all('div', class_='card-content')
 
-for job_element in job_elements:
-    title_element = job_element.find('h2', class_='title')
-    company_element = job_element.find('h3', class_='company')
-    location = job_element.find('p', class_='location')
-    # print(title_element.text.strip())
-    # print(company_element.text.strip())
-    # print(location.text.strip())
-    # print()
+# for job_element in job_elements:
+# title_element = job_element.find('h2', class_='title')
+# company_element = job_element.find('h3', class_='company')
+# location = job_element.find('p', class_='location')
+# print(title_element.text.strip())
+# print(company_element.text.strip())
+# print(location.text.strip())
+# print()
 
-python_jobs = results.find_all(
-    'h2', string=lambda text: 'python' in text.lower())
+# python_jobs = results.find_all(
+#     'h2', string=lambda text: 'python' in text.lower())
 
-for python_job in python_jobs:
-    parent_element = python_job.parent.parent.parent
-    title_element = parent_element.find('h2', class_='title')
-    company_element = parent_element.find('h3', class_='company')
-    location = parent_element.find('p', class_='location')
-    apply_link = parent_element.find_all('a')[1]
-    # print(title_element.text.strip())
-    # print(company_element.text.strip())
-    # print(location.text.strip())
-    # print(apply_link['href'].strip())
-    # print()
+# for python_job in python_jobs:
+# parent_element = python_job.parent.parent.parent
+# title_element = parent_element.find('h2', class_='title')
+# company_element = parent_element.find('h3', class_='company')
+# location = parent_element.find('p', class_='location')
+# apply_link = parent_element.find_all('a')[1]
+# print(title_element.text.strip())
+# print(company_element.text.strip())
+# print(location.text.strip())
+# print(apply_link['href'].strip())
+# print()
+
+
+def find_jobs_by_title(query):
+    jobs = results.find_all('h2', string=lambda text: query in text.lower())
+    for job in jobs:
+        parent_element = job.parent.parent.parent
+        title_element = parent_element.find('h2', class_='title')
+        company_element = parent_element.find('h3', class_='company')
+        location = parent_element.find('p', class_='location')
+        apply_link = parent_element.find_all('a')[1]
+        print(title_element.text.strip())
+        print(company_element.text.strip())
+        print(location.text.strip())
+        print(apply_link['href'].strip())
+        print()
+
+
+find_jobs_by_title(query='python')
